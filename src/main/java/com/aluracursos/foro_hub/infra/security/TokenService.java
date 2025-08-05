fd38933fafe73a496ac983d634aa7718a3ab333e
Cristina <cris78seijas@gmail.com>
2025-08-05 10:53:52 -0300

    se realizo la validacion del token recibido

--- src/main/java/com/aluracursos/foro_hub/infra/security/TokenService.java
+++ src/main/java/com/aluracursos/foro_hub/infra/security/TokenService.java
@@ -4,13 +4,13 @@ import com.aluracursos.foro_hub.domain.topico.usuario.Usuario;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import java.util.Date;
+
 
 @Service
 public class TokenService {
@@ -34,6 +34,21 @@ public class TokenService {
     private Instant fechaExpiracion() {
 
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API foro.hub")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+
+
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Token JWT invalido o expirado");
+        }
     }
 
 }
